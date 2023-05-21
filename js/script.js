5ff7c4b88093f81454a5ba561e118911569531ba
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-21 18:53:06 +0000

    init for webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function myButtonClicked() {
+// This function does calculates the hailstone sequence
+// input
+  let positiveInteger = parseInt(document.getElementById("positive-integer").value)
+
+// process
+  while (positiveInteger > 1) {
+    if (positiveInteger / 2 == 0) {
+      positiveInteger = positiveInteger / 2
+      document.getElementById("answer").innerHTML = positiveInteger
+    } else {
+      positiveInteger = positiveInteger * 3 + 1
+      document.getElementById("answer").innerHTML = positiveInteger
+    }
+  }
+
+// output
+  document.getElementById("answer").innerHTML = positiveInteger
+}
